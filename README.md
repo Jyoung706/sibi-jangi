ddc9947d44d6ca77a232b3d32b9138e0ac5fc443
jYoung706 <wnsdud9476@gmail.com>
2025-02-04 01:06:30 +0900

    project init

---
+++ README.md
@@ -0,0 +1,6 @@
+# 십이장기 온라인
+
+## 프로젝트 구조
+
+- `frontend/`: React + Vite 프론트엔드
+- `backend/`: NestJS 백엔드
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className='text-7xl'>십이 장기 온라인</div>
+    </>
+  );
+}
+
+export default App;
